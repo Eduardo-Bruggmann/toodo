d01c35bba50354cb8888e42a09bf76e1fe166952
Eduardo-Bruggmann <du.dudu.edu.5.ea@gmail.com>
2026-03-30 10:59:48 -0300

    refactor: update font family to Space Grotesk and add Card component

---
+++ src/components/Card.js
@@ -0,0 +1,7 @@
+export default function Card() {
+  return `
+        <div class="bg-purple-900 border border-purple-500/40 rounded-lg p-4">
+            Card Content
+        </div>
+    `
+}
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,11 @@
+import Card from './Card'
+
 export default function Main() {
   return `
         <main class="flex flex-1 flex-col justify-center items-center w-full gap-6 p-8 bg-[#111827]">
           <h1 class="text-xl font-bold text-[#F9FAFB]">Conteúdo Principal</h1>
 
+          <!--  
           <div class="grid w-full max-w-3xl grid-cols-1 gap-4 sm:grid-cols-2">
             <div class="rounded-lg p-6 bg-[#111827] border border-[#1F2937] text-[#F9FAFB]">
               Fundo Escuro
@@ -17,6 +20,9 @@ export default function Main() {
               Destaque
             </div>
           </div>
+          -->
+          
+          ${Card()}
         </main>
     `
 }
